fb0025aa9f49e7cd2be548bcfe4a66b5f73cdb63
rohan <rohan@Rohan05>
2023-09-01 17:23:00 +0530

    build 02

--- seluniumnew/src/xpath/Youtube.java
+++ seluniumnew/src/xpath/Youtube.java
@@ -12,7 +12,7 @@ public class Youtube {
 		  driver.get("http:www.youtube.com/");
 		  Thread.sleep(2000);
 	      driver.findElement(By.xpath("//input[@id='search']")).sendKeys("Chandrayan 3 launch");
-	      Thread.sleep(2000);
+	     // Thread.sleep(2000);
 	      driver.findElement(By.xpath("//button[@id='search-icon-legacy']")).click();
 		  Thread.sleep(2000);
 	      driver.findElement(By.xpath("//a[@id='video-title' and @title='Launch of LVM3-M4/CHANDRAYAAN-3 Mission from Satish Dhawan Space Centre (SDSC) SHAR, Sriharikota']")).click();
